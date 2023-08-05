67adc5c1025553e72dc425c2b438ad94f412de2b
Milton Salustiano <109237764+miltonneto10@users.noreply.github.com>
2023-08-05 15:27:31 -0300

    Commit Inicial

---
+++ OperadorLogicoNegacao.java
@@ -0,0 +1,19 @@
+/**
+Classe utilizada para demonstrar o uso do operador Lógico NEGAÇÃO ( ! )
+**/
+
+public class OperadorLogicoNegacao {
+	public static void main (String [] args) {
+		public static void main (String [] args) {
+			boolean a = true;
+			boolean b = false;
+			boolean c = false;
+			
+			System.out.println (!a);
+			System.out.println (!(b ||c));
+		}
+	}
+	
+/**
+O operador ! é chamado de “operador de negação”. Ele retornará true se o operador lógico for falso, assim como retornará false se o operador lógico for verdadeiro.
+**/
